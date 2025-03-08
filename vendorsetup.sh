66ded5f649bbf61e558d1a9e2fdc239253fd0d1d
xingrz <hi@xingrz.me>
2025-03-08 03:06:25 +0000

    ivan:releasetools: Ship and update vbmeta image

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #kernel
-git clone https://github.com/Notganesh/kernel_oneplus_ivan.git kernel/oplus/mt6877
+#git clone https://github.com/Notganesh/kernel_oneplus_ivan.git kernel/oplus/mt6877
 
 # Vendor
 git clone https://github.com/oneplus-mt6877/vendor_oneplus_ivan.git vendor/oneplus/ivan
